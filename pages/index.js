528673227b1933581a67f614894c28c3804cf522
DC-hashimoto-naoki <hashimoto_naoki@discovery-inc.com>
2022-12-25 16:20:40 +0900

    🐛 main and about page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import { Main } from './components/Main';
+import { Main } from './components/Main'
 
 const inter = Inter({ subsets: ['latin'] })
 
